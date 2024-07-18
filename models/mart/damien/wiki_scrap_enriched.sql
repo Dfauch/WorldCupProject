23a3b1845a7a04fd0ebfc2ed2f546777fbf3943d
Damien Faucherand <d.faucherand+1@gmail.com>
2024-07-18 14:46:19 +0000

    scrap wiki et players

--- models/mart/damien/wiki_scrap_enriched.sql
+++ models/mart/damien/wiki_scrap_enriched.sql
@@ -1,12 +1,12 @@
 with sofifa_enriched as (
     select
     date_team,
-    round(avg(age),2) as avg_age,
-    round(avg(market_value_in_eur),2) as avg_market_value,
-    round(avg(potential),2) as avg_potential,
-    round(avg(note),2) as avg_note,
-    round(avg(total_point),2) as avg_total_point,
-    round(avg(taille_in_cm),2) as avg_taille
+    cast(round(avg(age),2) as float64) as avg_age,
+    cast(round(avg(market_value_in_eur),2) as float64) as avg_market_value,
+    cast(round(avg(potential),2) as float64) as avg_potential,
+    cast(round(avg(note),2) as float64) as avg_note,
+    cast(round(avg(total_point),2) as float64) as avg_total_point,
+    cast(round(avg(taille_in_cm),2) as float64) as avg_taille
     from {{ref('join_wiki_scrap_sofifa')}} as sofifa
     GROUP by date_team
 
@@ -14,7 +14,7 @@ with sofifa_enriched as (
 sofifa_enriched_joined as (
     select
     sofifa_enriched.*,
-    avg_poste.*,
+    avg_poste.* except (year_country_key, date_team),
     from sofifa_enriched
     left join {{ref('joined_avg_age_poste')}} as avg_poste
     on sofifa_enriched.date_team = avg_poste.date_team
@@ -23,3 +23,4 @@ sofifa_enriched_joined as (
 Select
     *,
 from sofifa_enriched_joined
+where an > 2014
